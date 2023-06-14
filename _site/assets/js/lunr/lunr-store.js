a95d62b451aa417d5aaa268268c2707ecb4d258f
connorhmenzel <connorhmenzel@gmail.com>
2023-06-13 20:01:53 -0400

    Changed site organization

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "Proving that Reals are Uncountable (with Measure Theory)",
+        "excerpt":"I have been learning some basic measure theory this summer using the tail end Terence Tao’s book Analysis II. I have to know some fundamental results for the analysis comprehensive exam, so I figured I’d take some time (when I still have it) to study up. In exercise 7.2.6, he...","categories": [],
+        "tags": ["analysis","measure_theory"],
+        "url": "/reals_uncountable/",
+        "teaser": null
+      },{
+        "title": "Welcome!",
+        "excerpt":"Hi there! Welcome to my website. This site mainly serves as a personal blog of anything I find interesting as I embark on the journey of a math PhD program. I plan to write tons of math, for sure, but I also hope to include more personal posts where I...","categories": [],
+        "tags": ["introdution","personal"],
+        "url": "/welcome-to-my-blog/",
+        "teaser": null
+      }]
